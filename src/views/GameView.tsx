d9d38ab152e973a50363da74306f9d42cef8b054
Milan Gajic <boromirsrb@yahoo.com>
2025-01-26 11:22:26 +0100

    Adds info and links

--- src/views/GameView.tsx
+++ src/views/GameView.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState, type FC } from "react";
+import { Link } from "react-router";
+
 import { Card } from 'primereact/card';
 
 import CardBox from '../components/CardBox'
@@ -15,9 +17,8 @@ const GameView: FC = () => {
   const { handSolved, getAnswers } = game.handAndAnswers();
   const [hand, setHand] = useState<HandPoker>(handSolved)
   const [answers, setAnswers] = useState<string[]>(getAnswers)
-  // const [hand, setHand] = useState<HandPoker>()
-  // const [answers, setAnswers] = useState<string[]>([])
   const [isDirty, setIsDirty] = useState<boolean>(false)
+  const [suddenDeath, setSuddenDeath] = useState<number>(10)
   const [isCorrectAnswer, setIsCorrectAnswer] = useState<boolean>(false)
 
   const onAnswerSelected = (answer: string): void => {
@@ -44,7 +45,6 @@ const GameView: FC = () => {
   //     return;
   //   }
 
-
   // }, [timer.timesUp])
 
   const AnswerList = answers.map((answer: string, i: number) => {
@@ -59,18 +59,42 @@ const GameView: FC = () => {
 
   return (
     <Card className="flex items-center justify-center flex-col m-auto w-full max-w-sm md:max-w-xl rounded shadow-xl p-4 overflow-auto" style={{ height: "80vh" }} >
+      {/****** Timer ******/}
       <div
-        className="mb-10 order-1 text-5xl font-extrabold leading-none text-blue-300 dark:text-blue-300 text-right"
+        className={`${timer.counter < suddenDeath ? 'text-red-800 dark:text-red-800' : ''} mb-10 order-1 text-5xl font-extrabold leading-none text-blue-800 dark:text-blue-800 text-right`}
       >
         {timer.counter}
       </div>
+
+      {/****** Hand ******/}
       <div className="flex flex-wrap gap-3 justify-center">
         {hand && hand.cards.map((card: CardPoker, i: number) => (
           <CardBox cardType={card.suit} key={i}>{card.value}</CardBox>
         ))}
       </div >
 
-      <div className="mt-10 answers-list inline-block" >
+      {/******  Info ******/}
+      <div className={`${isCorrectAnswer && !timer.timesUp ? "text-green-500" : "text-red-500"} h-5 mt-4 font-bold text-center`}>
+        {isDirty && !timer.timesUp && (<span>{
+          isCorrectAnswer
+            ? `Correct!! You gain ${timer.step} seconds`
+            : `Wrong! You loose ${timer.step} seconds`
+        }</span>)}
+        {timer.timesUp && (<span>Game over :(</span>)}
+      </div>
+
+      {/****** Links ******/}
+      {timer.timesUp && <div className="mt-6 text-center">
+        <Link to="/leaderboard" className="block text-blue-400">
+          Go to Leaderboard
+        </Link>
+        <Link to="/" className="block mt-2 text-blue-400">
+          Start New Game
+        </Link>
+      </div>}
+
+      {/****** Answers ******/}
+      <div className={`${timer.timesUp ? 'game-over' : ''} mt-10 answers-list`}>
         {AnswerList}
       </div>
     </Card >
